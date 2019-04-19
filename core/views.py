204cb728b751911ce815c9aec9687fea9fd7713e
danilofaria <danilofaria.22@gmail.com>
2019-04-19 20:55:31 -0300

    index completo

--- core/views.py
+++ core/views.py
@@ -1,12 +1,34 @@
 from django.shortcuts import render
 from .models import *
 from django.shortcuts import render
+from django.db.models import Count
 # Create your views here.
 
 def index(request):
 
-    vagas = Vaga.objects.all()
+    vaga = Vaga.objects.all().order_by('salario')
 
-    return render(request, 'index.html', {
-        'vagas': vagas.order_by('salario')
-    })
+    vagaempresa = Vaga.objects.all(
+    ).order_by('empresa').values(
+        'empresa__nome'
+    ).annotate(count=Count('empresa__nome'))
+
+    vagaclt = Vaga.objects.filter(tipo_contrato='CLT'
+    ).order_by('titulo').values('titulo'
+    ).annotate(count=Count('titulo'))
+
+
+
+    vagapj = Vaga.objects.filter(tipo_contrato='PJ'
+    ).order_by('titulo').values('titulo'
+    ).annotate(count=Count('titulo'))
+
+
+    return render(request,'index.html', {
+        'vaga': vaga,
+        'vagaempresa': vagaempresa,
+        'vagaclt': vagaclt,
+        'vagapj': vagapj,
+
+
+})
