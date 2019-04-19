ecaeb080de020a370e64a1a54f9fc50749e0cf2a
danilofaria <danilofaria.22@gmail.com>
2019-04-19 14:53:32 -0300

    iniciando logicas de negocios

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+
+admin.site.site_header = 'Estamos Contratando'
+admin.site.site_title = ' WRH - Administração'
+
+class VagaAdmin(admin.ModelAdmin):
+        list_display = ('titulo','descricao','salario','empresa','requisitos')
+
+        list_filter = ('titulo','empresa','requisitos')
+
+        def empresa(self,obj):
+            return obj.empresa.nome
+
+        def requisitos(self,obj):
+            return obj.requisitos.requisitos_especificos
+
+class EmpresaAdmin(admin.ModelAdmin):
+
+        list_display =('nome','endereco','cidade')
+
+class RequisitosAdmin(admin.ModelAdmin):
+     list_display = ('requisitos_especificos','requisitos_gerais')
+
+
+
+admin.site.register(Vaga, VagaAdmin)
+admin.site.register(Empresa, EmpresaAdmin)
+admin.site.register(Requisitos, RequisitosAdmin)
--- core/views.py
+++ core/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from .models import *
+from django.shortcuts import render
 # Create your views here.
+
+def index(request):
+
+    vagas = Vaga.objects.all()
+
+    return render(request, 'index.html', {
+        'vagas': vagas.order_by('salario')
+    })
